4f13d1de9e36984e9acc0c1f882b8383d0932649
Michael Price <mtnp55@live.com>
2015-12-08 23:41:46 -0500

    UI improvements

--- client/templates/programs/program-page.js
+++ client/templates/programs/program-page.js
@@ -1,11 +1,50 @@
+Template.programPage.helpers({
+  memoryChecked: function () {
+    return this.brainTargets.indexOf("Memory") >= 0;
+  },
+  visuospartialChecked: function () {
+    return this.brainTargets.indexOf("Visuospartial") >= 0;
+  },
+  concentrationChecked: function () {
+    return this.brainTargets.indexOf("Concentration") >= 0;
+  },
+  orientationChecked: function () {
+    return this.brainTargets.indexOf("Orientation") >= 0;
+  },
+  languageChecked: function () {
+    return this.brainTargets.indexOf("Language") >= 0;
+  },
+  judgementChecked: function () {
+    return this.brainTargets.indexOf("Judgement") >= 0;
+  },
+  sequencingChecked: function () {
+    return this.brainTargets.indexOf("Sequencing") >= 0;
+  }
+})
+
 Template.programPage.events({
   "submit form": function (e) {
     e.preventDefault();
 
+    var filterObject = {
+        "Memory": $("#Memory-filter3").is(':checked'),
+        "Visuospartial": $("#Visuospartial-filter3").is(':checked'),
+        "Concentration": $("#Concentration-filter3").is(':checked'),
+        "Orientation": $("#Orientation-filter3").is(':checked'),
+        "Language": $("#Language-filter3").is(':checked'),
+        "Judgement": $("#Judgement-filter3").is(':checked'),
+        "Sequencing": $("#Sequencing-filter3").is(':checked')
+    };
+    var filterList = [];
+    for (filter in filterObject) {
+      if (filterObject[filter])
+        filterList.push(filter);
+    }
+
     var program = {
       title: $("#program-submit-title").val(),
       description: $("#program-submit-description").val(),
-      brainTargets: $("#program-submit-brain-targets").val().replace(/\s+/g, "").split(","),
+      brainTargets: filterList,
       tags: $("#program-submit-tags").val().replace(/\s+/g, "").split(","),
       documentLink: $("#program-submit-document-link").val(),
       tutorialLink: $("#program-submit-tutorial-link").val()
--- client/templates/programs/program-submit.js
+++ client/templates/programs/program-submit.js
@@ -2,10 +2,25 @@ Template.programSubmit.events({
   "submit form": function (e) {
     e.preventDefault();
 
+    var filterObject = {
+        "Memory": $("#Memory-filter2").is(':checked'),
+        "Visuospartial": $("#Visuospartial-filter2").is(':checked'),
+        "Concentration": $("#Concentration-filter2").is(':checked'),
+        "Orientation": $("#Orientation-filter2").is(':checked'),
+        "Language": $("#Language-filter2").is(':checked'),
+        "Judgement": $("#Judgement-filter2").is(':checked'),
+        "Sequencing": $("#Sequencing-filter2").is(':checked')
+    };
+    var filterList = [];
+    for (filter in filterObject) {
+      if (filterObject[filter])
+        filterList.push(filter);
+    }
+
     var program = {
       title: $("#program-submit-title").val(),
       description: $("#program-submit-description").val(),
-      brainTargets: $("#program-submit-brain-targets").val().replace(/\s+/g, "").split(","),
+      brainTargets: filterList,//$("#program-submit-brain-targets").val().replace(/\s+/g, "").split(","),
       tags: $("#program-submit-tags").val().replace(/\s+/g, "").split(","),
       documentLink: $("#program-submit-document-link").val(),
       tutorialLink: $("#program-submit-tutorial-link").val()
@@ -16,7 +31,7 @@ Template.programSubmit.events({
     Programs.insert(program, function (error, result) {
       if (error)
         return alert("An error occured");
-      Router.go("programPage", { _id: result });
+      Router.go("programDetails", { _id: result });
     });
   }
 });
