865fa3592ab9abe6d6855b55ccc25ee485084432
System Administrator <mtnp55@live.com>
2016-04-10 22:15:37 -0400

    Added documents to fixtures

--- server/startup/fixtures.js
+++ server/startup/fixtures.js
@@ -1,34 +1,99 @@
 Meteor.startup(function () {
   if (Activities.find().count() === 0) {
-    var now = new Date().getTime();
+    const now = new Date().getTime();
+    let interval = 0;
+    let tmpId = '';
+
+    console.log("Creating test users");
 
     // Insert test users
-    var user1Id = Meteor.users.insert({
+    const user1Id = Meteor.users.insert({
       profile: {
-        fullName: "Barack Obama",
+        fullName: "Rachel Witt",
         community: "Westminster",
       }
     });
-    var user2Id = Meteor.users.insert({
+    const user2Id = Meteor.users.insert({
       profile: {
-        fullName: "Joe Mama",
+        fullName: "Joe Trichak",
         community: "Westminster",
       }
     });
-    var user3Id = Meteor.users.insert({
+    const user3Id = Meteor.users.insert({
       profile: {
-        fullName: "Keegan Bruns",
+        fullName: "Andrew Heil",
         community: "Westminster",
       }
     });
-    var user4Id = Meteor.users.insert({
+    const user4Id = Meteor.users.insert({
       profile: {
-        fullName: "Ronald McDonald",
+        fullName: "John Doe",
         community: "Westminster",
       }
     });
 
-    // Test Activities
+    // Test Files
+    const fileObj1 = ActivityFiles.insert('assets/app/documents/frontal.parietal.food.3min.paper.docx');
+
+    console.log("Waiting on file 1...");
+    interval = setInterval(() => {
+      clearInterval(interval);
+    }, 2000);
+
+    const fileObj2 = ActivityFiles.insert('assets/app/documents/occipital.parietal.animal.45sec.paper.pub');
+
+    console.log("Waiting on file 2...");
+    interval = setInterval(() => {
+      clearInterval(interval);
+    }, 2000);
+
+    // Test Activities w/Files
+    const documentActivityIds = [];
+    [
+      {
+        title: "Food Activity",
+        description: "A test activity with documents.",
+        brainTargets: ['Frontal', 'Parietal'],
+        tags: ["food"],
+        tutorialLink: '',
+        documents: [fileObj1],
+        userId: user1Id,
+        created: now,
+        time: 3,
+        deliveryMethod: 'paper',
+      },
+      {
+        title: "Animal Activity",
+        description: "A test activity with documents.",
+        brainTargets: ['Occipital', 'Parietal'],
+        tags: ['animal'],
+        tutorialLink: '',
+        documents: [fileObj2],
+        userId: user1Id,
+        created: now,
+        time: 0.75,
+        deliveryMethod: 'paper',
+      },
+    ].forEach((activity) => {
+      tmpId = Activities.insert(activity);
+      documentActivityIds.push(activity);
+      console.log("Inserted Activity [" + tmpId + "]");
+    });
+
+    const documentProgram1 = {
+      title: "Example Program",
+      description: "A program consisting of two activities with documents.",
+      activityIds: documentActivityIds,
+      brainTargets: ["Frontal", "Parietal", "Occipital"],
+      tags: ["School", "TestProgram"],
+      tutorialLink: "https://www.youtube.com/embed/kMhw5MFYU0s",
+      userId: user1Id,
+      created: now
+    }
+    tmpId = Programs.insert(documentProgram1);
+    console.log("Inserted Document Program [" + tmpId + "]");
+
+    // Test Activities w/o Files
     [
       {
         title: "Frontal Activity",
@@ -40,6 +105,7 @@ Meteor.startup(function () {
         userId: user1Id,
         created: now,
         time: 1.5,
+        deliveryMethod: 'paper',
       },
       {
         title: "Parietal Activity",
@@ -51,6 +117,7 @@ Meteor.startup(function () {
         userId: user2Id,
         created: now,
         time: 2.6,
+        deliveryMethod: 'paper',
       },
       {
         title: "Temporal Activity",
@@ -62,6 +129,7 @@ Meteor.startup(function () {
         userId: user3Id,
         created: now,
         time: 0.6,
+        deliveryMethod: 'paper',
       },
       {
         title: "Occipital Activity",
@@ -73,9 +141,10 @@ Meteor.startup(function () {
         userId: user4Id,
         created: now,
         time: 3,
+        deliveryMethod: 'paper',
       },
     ].forEach((activity) => {
-      const tmpId = Activities.insert(activity);
+      tmpId = Activities.insert(activity);
       console.log("Inserted Activity [" + tmpId + "]");
     });
 
@@ -92,6 +161,7 @@ Meteor.startup(function () {
         userId: user1Id,
         created: now,
         time: 1.2,
+        deliveryMethod: 'paper',
       },
       {
         title: "Back to School Parietal Activity",
@@ -103,6 +173,7 @@ Meteor.startup(function () {
         userId: user3Id,
         created: now,
         time: 3,
+        deliveryMethod: 'paper',
       },
       {
         title: "Back to School Temporal Activity",
@@ -114,6 +185,7 @@ Meteor.startup(function () {
         userId: user2Id,
         created: now,
         time: 1.5,
+        deliveryMethod: 'paper',
       },
       {
         title: "Back to School Occipital Activity",
@@ -125,9 +197,10 @@ Meteor.startup(function () {
         userId: user1Id,
         created: now,
         time: 2.6,
+        deliveryMethod: 'paper',
       },
     ].forEach((activity) => {
-      const tmpId = Activities.insert(activity);
+      tmpId = Activities.insert(activity);
       linkedActivityIds.push(tmpId);
       console.log("Inserted Activity [" + tmpId + "]");
     });
@@ -136,13 +209,13 @@ Meteor.startup(function () {
       title: "Back to School",
       description: "A program containing 'back to school' themed activities",
       activityIds: linkedActivityIds,
-      brainTargets: ["Memory", "Judgement", "Concentration", "Sequencing"],
+      brainTargets: ["Frontal", "Parietal", "Temporal", "Occipital"],
       tags: ["School", "TestProgram"],
       tutorialLink: "https://www.youtube.com/embed/kMhw5MFYU0s",
       userId: user1Id,
       created: now
     }
-    const tmpId = Programs.insert(linkedProgram1);
-    console.log("Inserted Program [" + tmpId + "]");
+    tmpId = Programs.insert(linkedProgram1);
+    console.log("Inserted Linked Program [" + tmpId + "]");
   }
 });
